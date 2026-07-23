6c6aeb726a42007105821af848404a2946e8163d
nasky <nuridinawing121@gmail.com>
2026-07-23 13:57:25 +0700

    Add src/models/user.py

---
+++ src/models/user.py
@@ -0,0 +1,23 @@
+MongoDB user document defaults.
+
+DEFAULT_USER_DOC = {
+    "telegram_id": None,
+    "telegram_username": None,
+
+    "account_username": None,
+    "cookie_enc": None,
+    "cookie_verified_at": None,
+
+    "custom_bot_token_enc": None,
+    "group_chat_id": None,
+    "fingerprint": None,
+
+    "keywords": None,
+    "area": None,
+
+    "monitoring_active": False,
+    "checked_items": [],
+
+    "setup_state": None,
+    "setup_payload": {},
+}
